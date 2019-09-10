27775d4db9a929688b731d5aa176ed20908beb4f
LuMitchell <upmitchell@outlook.com>
2019-09-10 17:01:36 +0800

    进制转换

--- Common/Common.php
+++ Common/Common.php
@@ -16,3 +16,29 @@ public function fetchAllSeedByUserId($user_id)
     }
     return $seed;
 }
+
+//进制转换
+function from62to10($str){
+    $dict = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $len = strlen($str);
+    $dec = 0;
+    for($i = 0;$i<$len;$i++)
+    {
+        //找到对应字典的下标
+        $pos = strpos($dict, $str[$i]);
+        $dec += $pos * pow(62,$len-$i-1);
+    }
+    return $dec;
+}
+
+function from10to62($dec) {
+    $dict = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $result = '';
+    do
+    {
+        $result = $dict[$dec % 62] . $result;
+        $dec = intval($dec / 62);
+    } while ($dec != 0);
+    
+    return $result;
+}
