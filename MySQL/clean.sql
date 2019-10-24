11bb4b6ca2afc19216a4f82675e687439c4b577b
LuMitchell <upmitchell@outlook.com>
2019-10-24 16:53:26 +0800

    Update clean.sql

--- MySQL/clean.sql
+++ MySQL/clean.sql
@@ -4,3 +4,7 @@
 #执行500w需要6分钟左右（2CPU，4G）
 
 INSERT IGNORE INTO xx_copy (SELECT * FROM xx where id >= 5000000 and id < 10000000);
+
+
+## 将B表的某个字段的值更新到对应的A表里面的某个字段中
+update hospital_doctor as h, hospital_doctor_extra as ex set h.`summary` = ex.`summary` where h5.id = ex.id and ex.summary is not null;
